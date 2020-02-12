b582f9a75795944cb8ae37a5437505c9670730f3
Michal Piechowiak <misiek.piechowiak@gmail.com>
2020-02-12 04:52:41 +0100

    feat: support html img tags (#1)

--- plugin/src/index.js
+++ plugin/src/index.js
@@ -1,4 +1,5 @@
 const visit = require(`unist-util-visit`);
+const cheerio = require(`cheerio`);
 
 module.exports = ({ markdownAST }) => {
   visit(
@@ -10,4 +11,27 @@ module.exports = ({ markdownAST }) => {
       node.url = node.url.replace(`http://`, `https://`);
     }
   );
+  visit(markdownAST, [`html`, `jsx`], node => {
+    if (!node.value) {
+      return;
+    }
+
+    const $ = cheerio.load(node.value, {
+      xml: true
+    });
+    const images = $(`img`);
+    if (images.length === 0) {
+      return;
+    }
+
+    for (let i = 0; i < images.length; i++) {
+      const img = images[i];
+
+      if (img.attribs.src.startsWith(`http://`)) {
+        img.attribs.src = img.attribs.src.replace(`http://`, `https://`);
+      }
+    }
+
+    node.value = $.xml();
+  });
 };
